01d03b8c4d698133786a5c1cca2cc77b3100e2fd
Alicia <ladyeleniel@hotmail.com>
2024-07-04 16:59:27 +0000

    Add while loop

--- run.py
+++ run.py
@@ -1,3 +1,16 @@
-# Your code goes here.
-# You can delete these comments, but do not change the name of this file
-# Write your code to expect a terminal of 80 characters wide and 24 rows high
+def run():
+ 
+  bookArchive = []
+  choice = 0
+
+  while choice != 4:
+        print("Welcome to the Book Archives")
+        print("1. Add a new book")
+        print("2. Find a book")
+        print ("3. Display all books")
+        print("4. Exit")
+        choice = int(input())   
+
+if __name__ == "__main__":
+    run()
+
